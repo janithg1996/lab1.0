9756c2665ac4ac604fae4c361ce89f638f48128f
janithg1996 <janithgunasekara94@gmail.com>
2021-09-09 10:43:49 +0000

    this is a new commit

---
+++ access.py
@@ -0,0 +1,93 @@
+print("Hi! Welcome to kygeocloud")
+
+answer = input("Please enter your access code ")
+
+if answer =="10010":
+	print("Hello Janith! Have a good day")
+
+if answer =="10020":
+	print("Hello Manuka! Have a good day")
+
+if answer =="10030":
+	print("Hello Ayesh! Have a good day")
+
+if answer =="10040":
+	print("Hello Mayantha, Have a good day")
+
+if answer =="10050":
+	print("Hello Dushan! Have a good day")
+
+else:
+	print("Access denied")
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"access.py" 0L, 0C                                                                                                                                                                                                                                           0,0-1         All
+
